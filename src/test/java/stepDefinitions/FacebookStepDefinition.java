c95c7aa0db8707e1d4ebc27f6f43004ebd735529
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-03-28 23:44:14 +0300

    28/03/2023

---
+++ src/test/java/stepDefinitions/FacebookStepDefinition.java
@@ -0,0 +1,45 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import pages.FacebookPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class FacebookStepDefinition {
+    FacebookPage facebook=new FacebookPage();
+    Actions actions=new Actions(Driver.getDriver());
+    @Given("kullanici {string} ana sayfasinda")
+    public void kullaniciAnaSayfasinda(String istenenUrl) {
+        Driver.getDriver().get(ConfigReader.getProperty(istenenUrl));
+    }
+
+    @Then("Login yazisini tiklar")
+    public void loginYazisiniTiklar() {
+        facebook.login.click();
+    }
+
+    @And("gecersiz username girer")
+    public void gecersizUsernameGirer() {
+        facebook.email.sendKeys("gecersizgmail.com");
+    }
+    @And("gecersiz password girer")
+    public void gecersizPasswordGirer() {
+        actions.sendKeys(Keys.TAB).sendKeys("12345").perform();
+    }
+    @And("Login butonuna basar")
+    public void loginButonunaBasar() {
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+    }
+    @Then("sayfaya giris yapilamadigini kontrol eder")
+    public void sayfayaGirisYapilamadiginiKontrolEder() {
+        Assert.assertTrue(facebook.girisYapilmadi.isDisplayed());
+    }
+
+
+
+}
--- target/test-classes/features/US005_Facebook.feature
+++ target/test-classes/features/US005_Facebook.feature
@@ -1,8 +1,9 @@
 Feature: US005 Facebook Login
   Scenario: TC01 kullanici gecerli bilgilerle giris yapar
-    Given kullanici facebook ana sayfasinda
+    Given kullanici "faceUrl" ana sayfasinda
     Then Login yazisini tiklar
     And gecersiz username girer
+    And gecersiz password girer
     And Login butonuna basar
     Then sayfaya giris yapilamadigini kontrol eder
-    And kullanici sayfayi kapatir
+    And sayfayi kapatir
